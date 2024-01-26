863c75405a297db051153830e5cdd06b4cf5b9ce
BennXYZ <benny.zoeller@gmail.com>
2024-01-26 20:52:53 +0100

    Add Movement

--- Project/GGJ2024/Assets/PlayerMovement.cs
+++ Project/GGJ2024/Assets/PlayerMovement.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float cameraMovementSpeed;
-    public float playerMovementSpeed;
-    public float playerSpeedUpSpeed;
+    public float movementSpeed;
+    public float acceleration;
+    public float jumpForce;
     public float maxLowerCameraAngle;
     public float maxHigherCameraAngle;
     public Rigidbody rigidbody;
     public Transform cameraHolder;
+
+    Vector2 currentNormalizedSpeed;
 
     private void Awake()
     {
@@ -23,11 +26,32 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         //Debugging for me
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+            rigidbody.AddForce(Vector3.up * 100 * jumpForce);
+
+        if(acceleration != 0)
+        {
+            if (currentNormalizedSpeed.x > Input.GetAxis("Horizontal"))
+                currentNormalizedSpeed.x = Mathf.Max(currentNormalizedSpeed.x - acceleration * Time.deltaTime, Input.GetAxis("Horizontal"));
+            else if (currentNormalizedSpeed.x < Input.GetAxis("Horizontal"))
+                currentNormalizedSpeed.x = Mathf.Max(currentNormalizedSpeed.x + acceleration * Time.deltaTime, Input.GetAxis("Horizontal"));
+
+
+            if (currentNormalizedSpeed.y > Input.GetAxis("Vertical"))
+                currentNormalizedSpeed.y = Mathf.Max(currentNormalizedSpeed.y - acceleration * Time.deltaTime, Input.GetAxis("Vertical"));
+            else if (currentNormalizedSpeed.y < Input.GetAxis("Vertical"))
+                currentNormalizedSpeed.y = Mathf.Max(currentNormalizedSpeed.y + acceleration * Time.deltaTime, Input.GetAxis("Vertical"));
+
+            //currentNormalizedSpeed.x = Mathf.Clamp(currentNormalizedSpeed.x + Input.GetAxis("Horizontal") * (1 / Mathf.Abs(acceleration)) * Time.deltaTime, -movementSpeed, movementSpeed);
+            //currentNormalizedSpeed.y = Mathf.Clamp(currentNormalizedSpeed.y + Input.GetAxis("Vertical") * (1 / Mathf.Abs(acceleration)) * Time.deltaTime, -movementSpeed, movementSpeed);
+        }
+
         rigidbody.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * cameraMovementSpeed);
         float setoffEulerAngle = cameraHolder.transform.localEulerAngles.x;
         if (setoffEulerAngle > 180)
@@ -35,5 +59,10 @@ public class PlayerMovement : MonoBehaviour
         float targetRotation = setoffEulerAngle - Input.GetAxis("Mouse Y") * cameraMovementSpeed;
         targetRotation = Mathf.Clamp(targetRotation, maxLowerCameraAngle, maxHigherCameraAngle);
         cameraHolder.transform.Rotate(targetRotation - setoffEulerAngle, 0,0);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = Vector3.up * rigidbody.velocity.y + rigidbody.transform.forward * currentNormalizedSpeed.y * movementSpeed + rigidbody.transform.right * currentNormalizedSpeed.x * movementSpeed;
     }
 }
