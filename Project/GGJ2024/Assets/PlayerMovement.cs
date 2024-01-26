92b46b9074f0d3780dc697049ea72bb817c78be6
BennXYZ <benny.zoeller@gmail.com>
2024-01-26 20:15:27 +0100

    Add Camera Movement

--- Project/GGJ2024/Assets/PlayerMovement.cs
+++ Project/GGJ2024/Assets/PlayerMovement.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float cameraMovementSpeed;
+    public float playerMovementSpeed;
+    public float playerSpeedUpSpeed;
+    public float maxLowerCameraAngle;
+    public float maxHigherCameraAngle;
+    public Rigidbody rigidbody;
+    public Transform cameraHolder;
+
+    private void Awake()
+    {
+        if(!rigidbody)
+            rigidbody = GetComponent<Rigidbody>();
+        cameraHolder = transform.Find("CameraHolder");
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Debugging for me
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rigidbody.transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * cameraMovementSpeed);
+        float setoffEulerAngle = cameraHolder.transform.localEulerAngles.x;
+        if (setoffEulerAngle > 180)
+            setoffEulerAngle -= 360;
+        float targetRotation = setoffEulerAngle - Input.GetAxis("Mouse Y") * cameraMovementSpeed;
+        targetRotation = Mathf.Clamp(targetRotation, maxLowerCameraAngle, maxHigherCameraAngle);
+        cameraHolder.transform.Rotate(targetRotation - setoffEulerAngle, 0,0);
     }
 }
