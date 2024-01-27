72150ae850ef279e177c531350598a4e96da570e
MaxPlay <maxplayyt@gmail.com>
2024-01-27 14:25:34 +0100

    Added HUD

---
+++ Project/GGJ2024/Assets/Game/UI/HeadUpDisplay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+class HeadUpDisplay : MonoBehaviour
+{
+    [field:SerializeField]
+    public StealthStateDisplay StealthStateDisplay { get; private set; }
+
+    private void Start()
+    {
+        GameManager.Instance.CurrentLevel.OnStealthStateChanged.AddListener(StealthStateChanged);
+        StealthStateDisplay.SetStealthState(StealthState.Idle);
+    }
+
+    private void StealthStateChanged()
+    {
+        StealthStateDisplay.SetStealthState(GameManager.Instance.CurrentLevel.CurrentStealthState);
+    }
+}
