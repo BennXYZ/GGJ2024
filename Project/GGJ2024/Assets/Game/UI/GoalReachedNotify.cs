3e55a45985ed542c434b37eadf5ba936617f324d
MaxPlay <maxplayyt@gmail.com>
2024-01-27 22:25:18 +0100

    Added vault open notification and coin counter

---
+++ Project/GGJ2024/Assets/Game/UI/GoalReachedNotify.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class GoalReachedNotify : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI baseText;
+
+    [SerializeField]
+    TextMeshProUGUI animatedText;
+
+    [SerializeField]
+    AnimationCurve sizeCurve;
+
+    [SerializeField]
+    AnimationCurve alphaCurve;
+
+    [SerializeField]
+    float animationDuration;
+
+    bool show = false;
+
+    float animationTime;
+
+    private void Start()
+    {
+        baseText.enabled = false;
+        animatedText.enabled = false;
+    }
+
+    public void StartAnimation()
+    {
+        if (!show)
+        {
+            show = true;
+
+            baseText.enabled = true;
+            animatedText.enabled = true;
+            animationTime = 0;
+
+            StartCoroutine(PlayAnimation());
+        }
+    }
+    private IEnumerator PlayAnimation()
+    {
+        while (animationTime <= animationDuration)
+        {
+            animationTime += Time.deltaTime;
+
+            animatedText.fontSize = sizeCurve.Evaluate(animationTime / animationDuration);
+            Color color = animatedText.color;
+            color.a = alphaCurve.Evaluate(animationTime / animationDuration);
+            animatedText.color = color;
+            yield return null;
+        }
+
+        animatedText.enabled = false;
+    }
+}
--- Project/GGJ2024/Assets/Game/UI/HeadUpDisplay.cs
+++ Project/GGJ2024/Assets/Game/UI/HeadUpDisplay.cs
@@ -1,14 +1,35 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 class HeadUpDisplay : MonoBehaviour
 {
     [field:SerializeField]
     public StealthStateDisplay StealthStateDisplay { get; private set; }
 
+    [field:SerializeField]
+    public CoinCounterDisplay CoinCounterDisplay { get; private set; }
+    [field:SerializeField]
+    public GoalReachedNotify GoalReachedNotify { get; private set; }
+
     private void Start()
     {
-        GameManager.Instance.CurrentLevel.OnStealthStateChanged.AddListener(StealthStateChanged);
+        LevelRoot levelRoot = GameManager.Instance.CurrentLevel;
+        levelRoot.OnStealthStateChanged.AddListener(StealthStateChanged);
+        levelRoot.OnCoinsCollected.AddListener(CoinsCollected);
+        levelRoot.OnCoinCollectionGoalReached.AddListener(GoalReached);
+        CoinCounterDisplay.SetRequiredCount(levelRoot.CoinCollectionGoal);
+        CoinCounterDisplay.SetOwnedCount(levelRoot.CollectedCoins);
         StealthStateDisplay.SetStealthState(StealthState.Idle);
+    }
+
+    private void GoalReached()
+    {
+        GoalReachedNotify.StartAnimation();
+    }
+
+    private void CoinsCollected()
+    {
+        CoinCounterDisplay.SetOwnedCount(GameManager.Instance.CurrentLevel.CollectedCoins);
     }
 
     private void StealthStateChanged()
